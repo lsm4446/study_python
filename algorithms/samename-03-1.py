4ef52ae4ad6d637b3f2ca419adc623ec961a99e7
sangmyeon <sangmyeon@gmail.com>
2018-04-26 17:25:13 +0900

    집합 사용법 - 중복된 이름 찾

---
+++ algorithms/samename-03-1.py
@@ -0,0 +1,19 @@
+'''
+Created on 2018. 4. 26.
+
+@author: lsm
+'''
+def find_same_name(a):
+    n = len(a)
+    result = set()
+    for i in range(0, n-1):
+        for j in range(i+1, n):
+            if a[i] == a[j]:
+                result.add(a[i])
+    return result
+
+name = ["이상면", "Tom", "Jerry", "Mike", "이상면", "Tom"]
+print(find_same_name(name))
+
+name = ["이상면", "Tom", "Jerry", "Mike", "이상면", "Mike"]
+print(find_same_name(name))
